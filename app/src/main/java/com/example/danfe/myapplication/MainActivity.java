2ee44ccc887cfd92defc853cb2673822f74fc16d
Daniel Bedoya <danielbedoyatbs@utp.edu.co>
2018-09-17 09:45:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/danfe/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.danfe.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Palacios es gay", Toast.LENGTH_SHORT).show();
+    }
+}
